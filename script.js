03c45c75449bfa86394342fef81f69237e1ee41a
frayded <frayded99@gmail.com>
2024-08-13 22:18:10 +0700

    Added a let to call com choice as is

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// get randomized number from computer to be assigned to choices later
-// added number 1 for the Math.random() so it returns number 1 through 3
 function getComputerChoice() {
-  return Math.floor((Math.random() * 3) + 1);
+  let numOfChoices = 3;
+  return Math.floor((Math.random() * numOfChoices) + 1);
 }

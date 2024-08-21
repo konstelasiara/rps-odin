0f746ad53c27fd3e8981b2d2ce8e70335a7c3509
frayded <frayded99@gmail.com>
2024-08-21 10:11:23 +0700

    Added function for human selection

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function getComputerChoice() {
   let numOfChoices = 3;
   return Math.floor((Math.random() * numOfChoices) + 1);
 }
+
+function getHumanChoice() {
+  let humanChoice =  prompt("You may enter your choice").toLowerCase();
+  return humanChoice;
+}

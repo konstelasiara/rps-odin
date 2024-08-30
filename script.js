19311d0b2ff93f888ebce879c801c18b2ec9d477
frayded <frayded99@gmail.com>
2024-08-30 21:19:38 +0700

    A little msg

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+function getHumanChoice() {
+  let humanPrompt =  prompt("You may enter your choice");
+  return humanPrompt.charAt(0).toUpperCase() +
+  humanPrompt.slice(1).toLowerCase();
+}
+
 function getComputerChoice() {
   let numOfChoices = 3;
   return Math.floor((Math.random() * numOfChoices) + 1);
 }
 
-function getHumanChoice() {
-  let humanChoice =  prompt("You may enter your choice");
-  return humanChoice.charAt(0).toUpperCase() +
-  humanChoice.slice(1).toLowerCase();
+function playRound() {
+  /* 
+  possible to-dos:
+  - how to store a value in a function and make it usable anywhere?
+    - store it in a variable?
+  
+  */
+
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
 }
+
+// I have a massive burnout aaaaaa plssss get back to this later

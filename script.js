205dc01f94f492e5309e964ec518da20e3012752
konstelasiara <frayded99@gmail.com>
2024-11-28 13:35:04 +0700

    Finished main functionalities

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const scissors = document.querySelector("#scissors");
 const choices = document.querySelector("#choices");
 const outcome =  document.querySelector("#outcome");
 const score = document.querySelector("#score");
+const announce = document.querySelector("#announce");
 
+const reset = document.querySelector("#reset");
+
+
+// human choices are called here
 rock.addEventListener("click", () => playRound("Rock"));
 paper.addEventListener("click", () => playRound("Paper"))
 scissors.addEventListener("click", () => playRound("Scissors"));
@@ -15,6 +20,8 @@ let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 
+let gameOver = false;
+
 function getComputerChoice() {
   let numOfChoices = 3;
   let numRes = Math.floor(Math.random() * numOfChoices);
@@ -23,52 +30,63 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice) {
+  if (gameOver) {
+    return
+  }
+
   // trigger the choices functions
   let computerChoice = getComputerChoice();
 
   //retrieve the result on page
   choices.textContent = `You chose ${humanChoice}, Computer chose ${computerChoice}`;
 
-  let result = "";
+  let roundResult = "";
   // compare each choices
   if (humanChoice === "Rock" && computerChoice === "Scissors" ||
     humanChoice === "Paper" && computerChoice === "Rock" ||
     humanChoice === "Scissors" && computerChoice === "Paper") {
         humanScore++;
-        result = "Congrats! You win!";
+        roundResult = "Congrats! You win!";
     } else if (humanChoice === "Scissors" && computerChoice === "Rock" ||
       humanChoice === "Rock" && computerChoice === "Paper" ||
       humanChoice === "Paper" && computerChoice === "Scissors") {
         computerScore++;
-        result = "Uh oh! You lose!";
+        roundResult = "Uh oh! You lose!";
       } else {
         drawScore++;
-        result = "It's a draw!";
+        roundResult = "It's a draw!";
       }
 
-  outcome.textContent = result;
-  score.textContent = `You: ${humanScore} v. Computer: ${computerScore}`
+  outcome.textContent = roundResult;
+  score.textContent = `You: ${humanScore} v. Computer: ${computerScore}`;
+
+  if (humanScore == 5) {
+    announce.textContent = "You win the game!";
+    gameOver = true;
+  } else if (computerScore == 5) {
+    announce.textContent = "Computer wins the game!";
+    gameOver = true;
+  }
+
+  if (gameOver) {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
 }
 
-  
+reset.addEventListener("click", resetGame);
 
-//   // scoring
-//   console.log(`Your score: ${humanScore}`);
-//   console.log(`Computer score: ${computerScore}`);
-//   console.log(`Draw tally: ${drawScore}`);
-// }
-
-// declare game result
-// function gameResult() {
-//   if (humanScore > computerScore) {
-//     console.log("Congrats, you win the game!");
-//   } else if (computerScore > humanScore) {
-//     console.log("You lose... too bad...");
-//   } else {
-//     console.log("No one wins~");
-//   }
-// }
-
-
-// playGame();
-// gameResult();
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  drawScore = 0;
+  gameOver = false;
+  choices.textContent = "";
+  outcome.textContent = "";
+  score.textContent = "You: 0 v. Computer: 0";
+  announce.textContent = "";
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+}
